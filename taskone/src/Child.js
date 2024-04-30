e757e40891298a978410fc1cef6b6b3faab9c84d
zarinzaidi <aereinzayed@yahoo.com>
2024-04-30 11:01:02 +0800

    Child component update

--- taskone/src/Child.js
+++ taskone/src/Child.js
@@ -1,30 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 // 7.ChildComponent should:Receive the text passed from ParentComponent as a prop.
-function ChildComponent(props) {
-    const [name, setName] = useState();
-
-    //calling the getData fx from the parent comp n pass the 1st var name as arg to hold the data
-    props.getData(name);
-
+function ChildComponent({ text }) {
     return (
         // 8.Display the received text in table
         <div>
-            <h4>ChildComponent</h4>
             <table>
-                <tr>
-                    <th></th>
-                </tr>
-                {
-                    name.map((used) => (
-                        <tr>
-                            <td>{used.id}</td>
-                        </tr>
-                    ))
-                }
+                <thead>
+                    <tr>
+                        <th>Received Text</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{text}</td>
+                    </tr>
+                </tbody>
             </table>
         </div>
-    )
+    );
 }
+
 // 9.Implement the logic to pass the text from ParentComponent to ChildComponent.
-export default ChildComponent
+export default ChildComponent;
